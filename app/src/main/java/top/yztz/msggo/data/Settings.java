a40a42f20fbccd5c5a4d21e08122217a3af490af
kristianvv <kristianvaag@gmail.com>
2026-08-28 09:52:10 +0200

    Increased Excel sheet collumn limit to 1000

--- app/src/main/java/top/yztz/msggo/data/Settings.java
+++ app/src/main/java/top/yztz/msggo/data/Settings.java
@@ -39,8 +39,7 @@ public class Settings {
     public static final boolean DISCLAIMER_ACCEPTED_DEFAULT = false;
     // language
     public static final String LANGUAGE_DEFAULT = "auto";
-
-    public static final int EXCEL_ROW_COUNT_MAX = 200;
+    public static final int EXCEL_ROW_COUNT_MAX = 1000;
     public static final long EXCEL_FILE_SIZE_MAX = 50 * 1024 * 1024; // 50MB
 
 }
