0bea2ed6a781b18e73433b38bbe3b9fd61937256
kristianvv <kristianvaag@gmail.com>
2026-09-22 14:54:27 +0200

    Changed a few default settings to reflect the new changes for app

--- app/src/main/java/top/yztz/msggo/data/Settings.java
+++ app/src/main/java/top/yztz/msggo/data/Settings.java
@@ -24,16 +24,16 @@ public class Settings {
     public static final int SEND_DELAY_DEFAULT = 3000;
     public static final int SEND_DELAY_MIN = 1000;
     public static final int SEND_DELAY_MAX = 20000;
-    public static final long SEND_FINISH_DELAY_DEFAULT = 10000L;
-    public static final long SEND_FINISH_DELAY_MIN = 1000L;
-    public static final long SEND_FINISH_DELAY_MAX = 60000L;
+    public static final long SEND_FINISH_DELAY_DEFAULT = 100000L;
+    public static final long SEND_FINISH_DELAY_MIN = 10000L;
+    public static final long SEND_FINISH_DELAY_MAX = 600000L;
     public static final int SEND_FINISH_DELAY_STEP_UNIT = 1000;
     public static final int SEND_DELAY_STEP_UNIT = 500;
     
     // Response Listening Timeout (minutes)
-    public static final int LISTEN_TIMEOUT_DEFAULT = 10;
+    public static final int LISTEN_TIMEOUT_DEFAULT = 30;
     public static final int LISTEN_TIMEOUT_MIN = 1;
-    public static final int LISTEN_TIMEOUT_MAX = 30;
+    public static final int LISTEN_TIMEOUT_MAX = 60;
 
     public static final boolean SEND_DELAY_RANDOMIZATION_DEFAULT = true;
     // SMS rate
